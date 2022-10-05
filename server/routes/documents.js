e70548638fa0c3ac9433c9c6856f794c314780bd
Mohit Patil <mohit.patil@crimsoni.com>
2022-10-05 20:01:19 +0530

    [UPDATE] document update save to file logic added

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import axios from "axios";
-import FormData from "form-data";
-import AdmZip from "adm-zip";
-import fs from "fs";
+import axios from 'axios';
+import FormData from 'form-data';
+import AdmZip from 'adm-zip';
+import fs from 'fs';
 import upload from '../modules/file-upload.js';
 import * as formatConvert from '../modules/format-converter.js';
 
@@ -17,7 +17,9 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 router.get('/', (req, res, next) => {
   let listOfDocuments = [];
   fs.readdirSync('./document-store').forEach(file => {
-    listOfDocuments.push(file);
+    if(file.includes('.json')) {
+      listOfDocuments.push(file);
+    }
   });
 
   res.send({
@@ -33,7 +35,8 @@ router.get('/', (req, res, next) => {
 }*/
 router.post('/getById', async (req, res, next) => {
   var fileContents = fs.readFileSync(`./document-store/${req.body.id}`,'utf8');
-  let delta = formatConvert.convertHtmlToDelta(fileContents);
+  // let delta = formatConvert.convertHtmlToDelta(fileContents);
+  let delta = JSON.parse(fileContents);
 
   // update image src with server endpoint
   let listOfImageElements = delta.ops.map(item => item.insert).filter(item => typeof item === 'object' && item.image);
@@ -66,15 +69,21 @@ router.post('/upload', upload.single('file'), async (req, res, next) => {
 
     await delay(2*1000);
 
-    let fileName;
+    let newFileName;
     const zip = new AdmZip('./.temp/temp.zip');
     const zipEntries = zip.getEntries();
     zip.extractAllTo('./document-store', true);
 
     for (const zipEntry of zipEntries) {
       if (zipEntry.entryName.includes('.html')) {
-        fileName = zipEntry.entryName;
-        // let htmlContent = zipEntry.getData().toString('utf8');
+        let fileName = zipEntry.entryName;
+        let fileContents = zipEntry.getData().toString('utf8');
+        let delta = formatConvert.convertHtmlToDelta(fileContents);
+        let tempFileName = fileName.split('.');
+        tempFileName.pop();
+        newFileName = `${tempFileName.join('.')}.json`;
+        fs.writeFileSync(`./document-store/${newFileName}`, JSON.stringify(delta));
+
       } else if(zipEntry.entryName.includes('.png')) {
         let oldPath = `./document-store/${zipEntry.entryName}`;
         let newPath = `./assets/${zipEntry.entryName}`;
@@ -91,7 +100,7 @@ router.post('/upload', upload.single('file'), async (req, res, next) => {
     res.send({
       message: 'File uploaded successfully',
       status: true,
-      id: fileName
+      id: newFileName
     });
   })
   .catch((error) => {
